f87a69155fea5eb5556c8f62c42b34338f1f63f4
TuyisengeMaurice <62154173+TuyisengeMaurice@users.noreply.github.com>
2022-02-23 17:42:17 -0800

    Create gravity2.java

---
+++ gravity2.java
@@ -0,0 +1,11 @@
+class GravityCalculator {
+    public static void main(String[] arguments) {
+        double gravity = -9.81; // Earth's gravity in m/s^2
+        double initialVelocity = 0.0;
+        double fallingTime = 10.0;
+        double initialPosition = 10.0;
+        double finalPosition = 0.0;
+        double result=(0.5*(-9.81))*100;
+        System.out.println(result +" m.");
+    }
+}
